c3eafb353902f2fb19d09fba93f42207151e7bfe
Shirudo1 <liam@themartialmindset.com.au>
2023-11-07 11:55:35 +1100

    Following Along

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -19,3 +19,24 @@ customEmitter.on('response', () => {
 })
 
 customEmitter.emit('response', 'john', 34)
+
+// Event Driven Programming
+// Used heavily in Node.js
+// We listen for specific events, register functionsn that execute in response to those events.
+// Built in modules use events in Node.js
+
+// const EventEmitter = require('events');
+
+// const customEmitter = new EventEmitter();
+
+// // EvenetEmitter.on('response', () => {}) EventEmitter.Emit('')
+
+// // First listen for the event, and then emit it! The order matters.
+// customEmitter.on('response', (name, id) => {
+//     console.log(`data received user ${name} with id: ${id}`)
+// })
+// customEmitter.on('response', () => {
+//     console.log('some other logic here')
+// })
+
+// customEmitter.emit('response', 'john', 34)
